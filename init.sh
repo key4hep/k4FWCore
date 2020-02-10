e69606ce78999b0ec7a58223582005575f063e05
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-10 10:40:11 +0100

    add gaudi/cmake workarounds

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
+# see gh issue #2
 #source /cvmfs/sft.cern.ch/lcg/views/LCG_96c_LS/x86_64-centos7-gcc8-opt/setup.sh
+
 source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
+
+# workaround for a bug in the Gaudi CMake configuration,
+# can be removed in the future
+export Gaudi_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_96c_LS/Gaudi/v32r2/x86_64-centos7-gcc8-opt/
+
