7eda6953b5a337766c43c1efa3f9d67bd9bdc395
Valentin Volkl <valentin.volkl@cern.ch>
2020-04-15 03:11:20 +0200

    Update podio target name and use key4hep nightlies in init script

--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
-# see gh issue #2
-#source /cvmfs/sft.cern.ch/lcg/views/LCG_96c_LS/x86_64-centos7-gcc8-opt/setup.sh
 
-source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
-
-# workaround for a bug in the Gaudi CMake configuration,
-# can be removed in the future
-export Gaudi_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_96c_LS/Gaudi/v32r2/x86_64-centos7-gcc8-opt/
+source  /cvmfs/sw-nightlies.hsf.org/key4hep/views/latest/x86_64-centos7-gcc8-opt/setup.sh
 
