fac58fdf4e9f1b5721309ee207628440fc575262
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-10 16:47:58 +0100

    add downstream test
    debug ci
    
    [ci] works locally ...
    
    [ci] debug
    
    [ci] debug
    
    [ci] switch to github actions
    
    up
    
    up
    
    up
    
    up2

--- .k4fwcore-ci.d/compile_and_test.sh
+++
@@ -1,8 +0,0 @@
-
-source init.sh
-mkdir build install
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DHOST_BINARY_TAG=x86_64-linux-gcc9-opt .. && \
-make -j4 && \
-make install && \
-ctest --output-on-failure
