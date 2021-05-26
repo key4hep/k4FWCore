4b0b58f3db42da5469ac85475d6c075815ac010e
Valentin Volkl <valentin.volkl@cern.ch>
2021-05-26 09:22:31 +0200

    add test for command line arguments to k4run

--- test/k4TestFWCore/options/createHelloWorld.py
+++ test/k4TestFWCore/options/createHelloWorld.py
@@ -6,12 +6,12 @@ ApplicationMgr().EvtMax = 1
 ApplicationMgr().OutputLevel = INFO
 
 from Configurables import HelloWorldAlg
-producer = HelloWorldAlg()
+producer = HelloWorldAlg("HelloWorldAlg1")
 producer.PerEventPrintMessage = "Hello World !"
 ApplicationMgr().TopAlg += [producer]
 
 from Configurables import HelloWorldAlg
-producer2 = HelloWorldAlg()
+producer2 = HelloWorldAlg("HelloWorldAlg2")
 producer2.PerEventPrintMessage = "Hello World2 !"
 ApplicationMgr().TopAlg += [producer2]
 
