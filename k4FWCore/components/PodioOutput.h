7d51001d4b202d0977ae1b9cd86828fddfffe07e
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-06-18 13:09:58 +0900

    fix issue #44

--- k4FWCore/components/PodioOutput.h
+++ k4FWCore/components/PodioOutput.h
@@ -28,8 +28,8 @@ public:
   virtual StatusCode finalize();
 
 private:
-  void resetBranches(const std::vector<std::pair<std::string, podio::CollectionBase*>>& collections, bool prepare);
-  void createBranches(const std::vector<std::pair<std::string, podio::CollectionBase*>>& collections, bool prepare);
+  void resetBranches(const std::vector<std::pair<std::string, podio::CollectionBase*>>& collections);
+  void createBranches(const std::vector<std::pair<std::string, podio::CollectionBase*>>& collections);
   /// First event or not
   bool m_firstEvent;
   /// Root file name the output is written to
