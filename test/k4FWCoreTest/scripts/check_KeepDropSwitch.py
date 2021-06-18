be87a06976460229ac435f2817249b8a2b7ef16a
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-06-18 18:59:47 +0900

    make the test to fail properly

--- test/k4FWCoreTest/scripts/check_KeepDropSwitch.py
+++ test/k4FWCoreTest/scripts/check_KeepDropSwitch.py
@@ -4,16 +4,11 @@ file = ROOT.TFile.Open("output_k4test_exampledata_2.root")
 tree = file.Get("events")
 tree.GetEntry(0)
 
-try:
-  ndf = tree.Tracks.at(0).ndf
-  if ndf==0:
-    raise Exception('cannot retrieve Track.ndf')
-except Exception as IOError:
-  print('podio::CollectionBase read from file did not saved properly')
+ndf = tree.Tracks.at(0).ndf
+if ndf==0:
+  raise Exception('podio::CollectionBase read from file did not saved properly')
 
-try:
-  status = tree.GetBranchStatus("MCParticles")
-  if status==True:
-    raise Exception('MCParticles did not drop')
-except Exception as IOError:
-  print('Drop switch for PodioOutput does not work properly') 
+status = tree.GetBranchStatus("MCParticles")
+if status==True:
+  raise Exception('KeepDropSwitch did not drop the collection')
+
