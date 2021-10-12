95b0b512869c4d2ed3a31cac2c2fc27dd3963ed6
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-10-12 10:23:29 +0200

    separate cellID const for testing

--- test/k4FWCoreTest/src/components/k4FWCoreTest_cellID_writer.h
+++ test/k4FWCoreTest/src/components/k4FWCoreTest_cellID_writer.h
@@ -11,6 +11,8 @@
 // datamodel
 #include "edm4hep/SimTrackerHitCollection.h"
 
+const std::string cellIDtest = "M:3,S-1:3,I:9,J:9,K-1:6";
+
 /** @class k4FWCoreTest_cellID
  *  Lightweight producer for edm data to test cellID 
  */
