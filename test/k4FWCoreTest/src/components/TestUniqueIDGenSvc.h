2c0fd69c794688cd010771622b050a7b04836cfb
Placido Fernandez Declara <placido.fernandez@cern.ch>
2022-04-19 18:08:20 +0200

    add test for unique id service

---
+++ test/k4FWCoreTest/src/components/TestUniqueIDGenSvc.h
@@ -0,0 +1,31 @@
+#ifndef TEST_UNIQUEIDGENSVC_H
+#define TEST_UNIQUEIDGENSVC_H
+
+// GAUDI
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+
+#include <k4FWCore/IUniqueIDGenSvc.h>
+
+class TestUniqueIDGenSvc : public GaudiAlgorithm {
+public:
+  explicit TestUniqueIDGenSvc(const std::string&, ISvcLocator*);
+  virtual ~TestUniqueIDGenSvc();
+  /**  Initialize.
+   *   @return status code
+   */
+  virtual StatusCode initialize() final;
+  /**  Execute.
+   *   @return status code
+   */
+  virtual StatusCode execute() final;
+  /**  Finalize.
+   *   @return status code
+   */
+  virtual StatusCode finalize() final;
+
+private:
+  SmartIF<IUniqueIDGenSvc> m_service;
+};
+
+#endif // TEST_UNIQUEIDGENSVC_H 
