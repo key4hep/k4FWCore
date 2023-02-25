27206aa266e37b2f6f0e87c69f6312e0b3942d6a
Gerardo Ganis <gerardo.ganis@cern.ch>
2023-02-25 13:33:30 +0100

    Fix warnings due to missing override specification

--- k4FWCore/include/k4FWCore/PodioDataSvc.h
+++ k4FWCore/include/k4FWCore/PodioDataSvc.h
@@ -22,10 +22,10 @@ class PodioDataSvc : public DataSvc {
 public:
   typedef std::vector<std::pair<std::string, podio::CollectionBase*>> CollRegistry;
 
-  virtual StatusCode initialize();
-  virtual StatusCode reinitialize();
-  virtual StatusCode finalize();
-  virtual StatusCode clearStore();
+  virtual StatusCode initialize() override;
+  virtual StatusCode reinitialize() override;
+  virtual StatusCode finalize() override;
+  virtual StatusCode clearStore() override;
 
   /// Standard Constructor
   PodioDataSvc(const std::string& name, ISvcLocator* svc);
