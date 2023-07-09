89c1dfe82e6eae473c5fbeb14099910111499341
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-09 17:38:38 +0200

    Up

--- test/k4FWCoreTest/options/runExampleProducer.py
+++ test/k4FWCoreTest/options/runExampleProducer.py
@@ -3,11 +3,20 @@ from Configurables import ExampleProducer
 from Configurables import EvtStoreSvc
 from Configurables import ApplicationMgr
 
+from Configurables import k4DataSvc
+podioevent = k4DataSvc("EventDataSvc")
+ApplicationMgr().ExtSvc += [podioevent]
+
+from Configurables import PodioOutput
+out = PodioOutput("out")
+out.filename = "output_k4test_exampledata_twoproducer.root"
+out.outputCommands = ["keep *"]
+
 producer = ExampleProducer("ExampleProducer", OutputLocation="/ExampleInt", ExampleInt=5)
 
-ApplicationMgr( TopAlg=[producer],
+ApplicationMgr( TopAlg=[producer, out],
                 EvtSel="NONE",
                 EvtMax=10,
-                ExtSvc=[EvtStoreSvc("EventDataSvc")],
+                ExtSvc=[k4DataSvc("EventDataSvc")],
                 OutputLevel=INFO,
                 )
