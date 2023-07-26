b408e13319a1d0a3940a15bfd1e0540c5be3ca27
Andre Sailer <andre.philippe.sailer@cern.ch>
2023-07-26 10:44:36 +0200

    Add Apache License

--- k4FWCore/components/k4LegacyDataSvc.h
+++ k4FWCore/components/k4LegacyDataSvc.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2014-2023 Key4hep-Project.
+ *
+ * This file is part of Key4hep.
+ * See https://key4hep.github.io/key4hep-doc/ for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef K4FWCORE_K4LEGACYDATASVC_H
 #define K4FWCORE_K4LEGACYDATASVC_H
