28a35c171d9d0928185e07960a586cf176dc7987
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-26 21:28:00 +0200

    Update the example producer

--- test/k4FWCoreTest/options/runExampleProducer.py
+++ test/k4FWCoreTest/options/runExampleProducer.py
@@ -5,18 +5,18 @@ from Configurables import ApplicationMgr
 
 from Configurables import k4DataSvc
 podioevent = k4DataSvc("EventDataSvc")
-ApplicationMgr().ExtSvc += [podioevent]
 
 from Configurables import PodioOutput
 out = PodioOutput("out")
-out.filename = "output_k4test_exampledata_twoproducer.root"
+out.filename = "output_k4test_exampledata_producer.root"
 out.outputCommands = ["keep *"]
 
-producer = FunctionalProducer("FunctionalProducer", OutputLocation="/ExampleInt", ExampleInt=5)
+producer = FunctionalProducer("FunctionalProducer",
+                              OutputLocation="ExampleInt", ExampleInt=5)
 
-ApplicationMgr( TopAlg=[producer, out],
-                EvtSel="NONE",
-                EvtMax=10,
-                ExtSvc=[k4DataSvc("EventDataSvc")],
-                OutputLevel=INFO,
-                )
+ApplicationMgr(TopAlg=[producer, out],
+               EvtSel="NONE",
+               EvtMax=10,
+               ExtSvc=[k4DataSvc("EventDataSvc")],
+               OutputLevel=INFO,
+               )
--- test/k4FWCoreTest/src/components/ExampleProducer.cpp
+++ test/k4FWCoreTest/src/components/ExampleProducer.cpp
@@ -1,20 +1,58 @@
 #include "Gaudi/Property.h"
 #include "GaudiAlg/GaudiAlgorithm.h"
 #include "GaudiAlg/Producer.h"
+#include "edm4hep/MCParticleCollection.h"
+#include "podio/UserDataCollection.h"
+#include "k4FWCore/DataHandle.h"
 
 #include <string>
 
 using BaseClass_t = Gaudi::Functional::Traits::BaseClass_t<Gaudi::Algorithm>;
 
-struct FunctionalProducer final : Gaudi::Functional::Producer<int(), BaseClass_t> {
+
+using colltype = DataWrapper<podio::UserDataCollection<float>>;
+
+struct FunctionalProducer final : Gaudi::Functional::Producer<colltype(), BaseClass_t> {
+
+  mutable DataHandle<colltype> m_vectorFloatHandle{"VectorFloat", Gaudi::DataHandle::Writer, this};
 
   FunctionalProducer( const std::string& name, ISvcLocator* svcLoc )
     : Producer( name, svcLoc, KeyValue( "OutputLocation", "/ExampleInt" ) ) {}
 
-  int operator()() const override {
-    info() << "Producing " << m_exampleInt << endmsg;
-    return m_exampleInt;
+  colltype operator()() const override {
+    // auto* res = m_vectorFloatHandle.createAndPut();
+     // res{};
+
+    auto coll = new podio::UserDataCollection<float>();
+    coll->push_back(1.5);
+    colltype* dw = new DataWrapper<podio::UserDataCollection<float>>();
+    dw->setData(coll);
+    return *dw;
+    // return m_exampleInt;
   }
+
+// using colltype = podio::UserDataCollection<float>;
+
+// struct FunctionalProducer final : Gaudi::Functional::Producer<colltype(), BaseClass_t> {
+
+//   mutable DataHandle<colltype> m_vectorFloatHandle{"VectorFloat", Gaudi::DataHandle::Writer, this};
+
+//   FunctionalProducer( const std::string& name, ISvcLocator* svcLoc )
+//     : Producer( name, svcLoc, KeyValue( "OutputLocation", "/ExampleInt" ) ) {}
+
+//   colltype operator()() const override {
+//     // auto* res = m_vectorFloatHandle.createAndPut();
+//      // res{};
+
+//     // auto coll = new podio::UserDataCollection<float>();
+//     // coll->push_back(1.0);
+//     auto coll = podio::UserDataCollection<float>();
+//     coll.push_back(2.5);
+//     // colltype* dw = new DataWrapper<podio::UserDataCollection<float>>();
+//     // dw->setData(coll);
+//     return coll;
+//     // return m_exampleInt;
+//   }
 
   Gaudi::Property<int> m_exampleInt{this, "ExampleInt", 3,
                                     "Example int to be produced"};
