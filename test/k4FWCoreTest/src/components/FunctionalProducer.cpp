5650e445d868dd657fc88f1949d4bad70d9f66f7
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-26 21:30:39 +0200

    Change to Functional Producer

--- test/k4FWCoreTest/src/components/FunctionalProducer.cpp
+++ test/k4FWCoreTest/src/components/FunctionalProducer.cpp
@@ -9,7 +9,6 @@
 
 using BaseClass_t = Gaudi::Functional::Traits::BaseClass_t<Gaudi::Algorithm>;
 
-
 using colltype = DataWrapper<podio::UserDataCollection<float>>;
 
 struct FunctionalProducer final : Gaudi::Functional::Producer<colltype(), BaseClass_t> {
@@ -20,39 +19,13 @@ struct FunctionalProducer final : Gaudi::Functional::Producer<colltype(), BaseCl
     : Producer( name, svcLoc, KeyValue( "OutputLocation", "/ExampleInt" ) ) {}
 
   colltype operator()() const override {
-    // auto* res = m_vectorFloatHandle.createAndPut();
-     // res{};
-
     auto coll = new podio::UserDataCollection<float>();
     coll->push_back(1.5);
+    coll->push_back(2.5);
     colltype* dw = new DataWrapper<podio::UserDataCollection<float>>();
     dw->setData(coll);
     return *dw;
-    // return m_exampleInt;
   }
-
-// using colltype = podio::UserDataCollection<float>;
-
-// struct FunctionalProducer final : Gaudi::Functional::Producer<colltype(), BaseClass_t> {
-
-//   mutable DataHandle<colltype> m_vectorFloatHandle{"VectorFloat", Gaudi::DataHandle::Writer, this};
-
-//   FunctionalProducer( const std::string& name, ISvcLocator* svcLoc )
-//     : Producer( name, svcLoc, KeyValue( "OutputLocation", "/ExampleInt" ) ) {}
-
-//   colltype operator()() const override {
-//     // auto* res = m_vectorFloatHandle.createAndPut();
-//      // res{};
-
-//     // auto coll = new podio::UserDataCollection<float>();
-//     // coll->push_back(1.0);
-//     auto coll = podio::UserDataCollection<float>();
-//     coll.push_back(2.5);
-//     // colltype* dw = new DataWrapper<podio::UserDataCollection<float>>();
-//     // dw->setData(coll);
-//     return coll;
-//     // return m_exampleInt;
-//   }
 
   Gaudi::Property<int> m_exampleInt{this, "ExampleInt", 3,
                                     "Example int to be produced"};
