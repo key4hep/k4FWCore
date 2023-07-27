6f8910e1cdc9e124a4f644440e2e4579cc0f6382
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-27 14:30:32 +0200

    Move PodioInput to use Gaudi::Functional

--- k4FWCore/components/PodioInput.cpp
+++ k4FWCore/components/PodioInput.cpp
@@ -17,49 +17,29 @@
  * limitations under the License.
  */
 #include "PodioInput.h"
+#include "GaudiAlg/Consumer.h"
 
-#include "TFile.h"
-#include "TROOT.h"
-
-#include "k4FWCore/DataWrapper.h"
 #include "k4FWCore/PodioDataSvc.h"
 
 DECLARE_COMPONENT(PodioInput)
 
-PodioInput::PodioInput(const std::string& name, ISvcLocator* svcLoc) : GaudiAlgorithm(name, svcLoc) {}
-
-StatusCode PodioInput::initialize() {
-  if (GaudiAlgorithm::initialize().isFailure())
-    return StatusCode::FAILURE;
-
+PodioInput::PodioInput(const std::string& name, ISvcLocator* svcLoc) : Consumer(name, svcLoc) {
   // check whether we have the PodioEvtSvc active
   m_podioDataSvc = dynamic_cast<PodioDataSvc*>(evtSvc().get());
-  if (nullptr == m_podioDataSvc)
-    return StatusCode::FAILURE;
-
-  // TODO: add an upfront check for existence of data products
-
-  return StatusCode::SUCCESS;
+  if (!m_podioDataSvc) {
+    error() << "Could not get PodioDataSvc" << endmsg;
+  }
 }
 
-StatusCode PodioInput::execute() {
-  size_t cntr = 0;
-  // Re-create the collections from ROOT file
+void PodioInput::operator()() const {
 
   for (auto& collName : m_collectionNames) {
     debug() << "Registering collection to read " << collName << endmsg;
     if (m_podioDataSvc->readCollection(collName).isFailure()) {
-      return StatusCode::FAILURE;
+      error() << "Failed to register collection " << collName << endmsg;
     }
   }
 
   // Tell data service that we are done with requested collections
   m_podioDataSvc->endOfRead();
-  return StatusCode::SUCCESS;
-}
-
-StatusCode PodioInput::finalize() {
-  if (GaudiAlgorithm::finalize().isFailure())
-    return StatusCode::FAILURE;
-  return StatusCode::SUCCESS;
 }
