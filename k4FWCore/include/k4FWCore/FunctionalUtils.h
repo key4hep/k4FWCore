80f48c81ca2231d04a714d0b7bd66992c8f0747b
jmcarcell <jmcarcell@users.noreply.github.com>
2023-08-08 18:02:16 +0200

    Improve the interface for producers

---
+++ k4FWCore/include/k4FWCore/FunctionalUtils.h
@@ -0,0 +1,19 @@
+#ifndef K4FWCORE_FUNCTIONALUTILS_H
+#define K4FWCORE_FUNCTIONALUTILS_H
+
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiKernel/DataObjectHandle.h"
+#include "k4FWCore/DataWrapper.h"
+
+// Base class used for the Traits template argument of the
+// Gaudi::Functional algorithms
+struct BaseClass_t {
+  template<typename T>
+  using OutputHandle = DataObjectWriteHandle<DataWrapper<T>>;
+  // template<typename T>
+  // using InputHandle = DataObjectReadHandle<DataWrapper<T>>;
+  
+  using BaseClass = Gaudi::Algorithm;
+};
+
+#endif
