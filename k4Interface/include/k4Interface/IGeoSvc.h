93aba99a8a6add425593d20d5c58e7095986a538
Juraj Smiesko <juraj.smiesko@cern.ch>
2024-02-08 11:21:10 +0100

    Formatting

--- k4Interface/include/k4Interface/IGeoSvc.h
+++ k4Interface/include/k4Interface/IGeoSvc.h
@@ -32,10 +32,10 @@ class G4VUserDetectorConstruction;
 class GAUDI_API IGeoSvc : virtual public IService {
 public:
   DeclareInterfaceID(IGeoSvc, 1, 0);
-  virtual dd4hep::DetElement                                            getDD4HepGeo()                            = 0;
-  virtual dd4hep::Detector*                                             getDetector()                             = 0;
-  virtual G4VUserDetectorConstruction*                                  getGeant4Geo()                            = 0;
-  virtual std::string                                                   constantAsString(std::string const& name) = 0;
+  virtual dd4hep::DetElement           getDD4HepGeo()                            = 0;
+  virtual dd4hep::Detector*            getDetector()                             = 0;
+  virtual G4VUserDetectorConstruction* getGeant4Geo()                            = 0;
+  virtual std::string                  constantAsString(std::string const& name) = 0;
   virtual ~IGeoSvc() {}
 };
 
