2d282f53dc5b559f6fa400da750f93d695c7b4ff
Thomas Madlener <thomas.madlener@desy.de>
2024-02-26 08:22:00 +0100

    Fix deprecation warnings for changed mutable accessors (#181)

--- test/k4FWCoreTest/src/components/k4FWCoreTest_AlgorithmWithTFile.cpp
+++ test/k4FWCoreTest/src/components/k4FWCoreTest_AlgorithmWithTFile.cpp
@@ -56,7 +56,7 @@ StatusCode k4FWCoreTest_AlgorithmWithTFile::execute() {
 
   auto particle = particles->create();
 
-  auto& p4 = particle.momentum();
+  auto& p4 = particle.getMomentum();
   p4.x     = m_magicNumberOffset + 5;
   p4.y     = m_magicNumberOffset + 6;
   p4.z     = m_magicNumberOffset + 7;
