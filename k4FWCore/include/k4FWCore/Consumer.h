f23ad30ce1671def3a42ae342a04fb3eac1cf301
jmcarcell <jmcarcell@users.noreply.github.com>
2024-03-08 13:26:15 +0100

    Move readMapInputs to FunctionalUtils.h

--- k4FWCore/include/k4FWCore/Consumer.h
+++ k4FWCore/include/k4FWCore/Consumer.h
@@ -89,47 +89,10 @@ namespace k4FWCore {
                Gaudi::Functional::details::RepeatValues_<KeyValues, sizeof...(In)> const& inputs)
           : Consumer(std::move(name), locator, inputs, std::index_sequence_for<In...>{}) {}
 
-      template <size_t Index, typename... Handles> void readMapInputs(const std::tuple<Handles...>& handles) const {
-        if constexpr (Index < sizeof...(Handles)) {
-          if constexpr (is_map_like<std::tuple_element_t<Index, std::tuple<In...>>>::value) {
-            // In case of map types like std::map<std::string, edm4hep::MCParticleCollection&>
-            // we have to remove the reference to get the actual type
-            using EDM4hepType =
-                std::remove_reference_t<typename std::tuple_element_t<Index, std::tuple<In...>>::mapped_type>;
-            auto inputMap = std::map<std::string, const EDM4hepType&>();
-
-            // To be locked
-            if (!m_inputLocationsMap.contains(std::get<Index>(handles).objKey())) {
-              auto vec = std::vector<std::string>();
-              vec.reserve(m_inputLocations[Index].value().size());
-              for (auto& val : m_inputLocations[Index].value()) {
-                vec.push_back(val.key());
-              }
-              m_inputLocationsMap[std::get<Index>(handles).objKey()] = vec;
-            }
-
-            for (auto& value : m_inputLocationsMap.at(std::get<Index>(handles).objKey())) {
-              DataObject* p;
-              auto        sc = this->evtSvc()->retrieveObject(value, p);
-              if (!sc.isSuccess()) {
-                throw GaudiException("Failed to retrieve object " + value, "Consumer", StatusCode::FAILURE);
-              }
-              const auto collection = dynamic_cast<AnyDataWrapper<std::shared_ptr<podio::CollectionBase>>*>(p);
-              auto       ptr        = std::dynamic_pointer_cast<EDM4hepType>(collection->getData());
-              inputMap.emplace(value, *ptr);
-            }
-            std::get<Index>(handles).put(std::move(inputMap));
-          }
-
-          // Recursive call for the next index
-          readMapInputs<Index + 1>(handles);
-        }
-      }
-
       // derived classes are NOT allowed to implement execute ...
       StatusCode execute(const EventContext& ctx) const override final {
         try {
-          readMapInputs<0>(this->m_inputs);
+          readMapInputs<0, In...>(this->m_inputs, m_inputLocations, m_inputLocationsMap, this);
           filter_evtcontext_tt<In...>::apply(*this, ctx, this->m_inputs);
           return Gaudi::Functional::FilterDecision::PASSED;
         } catch (GaudiException& e) {
