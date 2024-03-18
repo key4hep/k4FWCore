096527e9713545411935efa3e4ccc78218daa201
jmcarcell <jmcarcell@users.noreply.github.com>
2024-03-18 15:21:42 +0100

    Use setMomentum for particles

--- test/k4FWCoreTest/src/components/ExampleFunctionalProducerMultiple.cpp
+++ test/k4FWCoreTest/src/components/ExampleFunctionalProducerMultiple.cpp
@@ -68,10 +68,7 @@ struct ExampleFunctionalProducerMultiple final
 
     auto  particles = edm4hep::MCParticleCollection();
     auto  particle  = particles.create();
-    auto& p4        = particle.getMomentum();
-    p4.x            = m_magicNumberOffset + m_event + 5;
-    p4.y            = m_magicNumberOffset + 6;
-    p4.z            = m_magicNumberOffset + 7;
+    particle.setMomentum({m_magicNumberOffset + m_event + 5.0, m_magicNumberOffset + 6.0, m_magicNumberOffset + 7.0});
     particle.setMass(m_magicNumberOffset + m_event + 8);
 
     auto simTrackerHits = edm4hep::SimTrackerHitCollection();
