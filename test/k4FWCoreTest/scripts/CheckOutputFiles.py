6dfbddb5dcd607b67e4b9d3e690e78531a9396c1
Thomas Madlener <thomas.madlener@desy.de>
2025-02-19 15:07:27 +0100

    Keep backwards compatibility with older podio versions

--- test/k4FWCoreTest/scripts/CheckOutputFiles.py
+++ test/k4FWCoreTest/scripts/CheckOutputFiles.py
@@ -120,8 +120,9 @@ check_collections(
     ],
 )
 
-check_collections("output_k4test_exampledata_limited.root", ["MCParticles", "Links"])
-check_collections("functional_limited_input.root", ["MCParticles", "Links"])
+if podio.version.build_version > podio.version.parse("1.2.0"):
+    check_collections("output_k4test_exampledata_limited.root", ["MCParticles", "Links"])
+    check_collections("functional_limited_input.root", ["MCParticles", "Links"])
 
 mix_collections = [
     # From file
