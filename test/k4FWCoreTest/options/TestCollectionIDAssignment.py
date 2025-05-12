4c9f0edc5e4c99465b2e9db8e5975892c4d8efd3
Thomas Madlener <thomas.madlener@desy.de>
2025-05-12 20:03:02 +0200

    Add tests for checking the collection ID

---
+++ test/k4FWCoreTest/options/TestCollectionIDAssignment.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2014-2024 Key4hep-Project.
+#
+# This file is part of Key4hep.
+# See https://key4hep.github.io/key4hep-doc/ for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This is a simple test checking that the collection IDs that are assigned for
+# the Functional algorithms are the same as we get when adding to a frame.
+# Effectively, it checks whether we hash the right thing when we assign a
+# collection ID. We need this check because for Functional algorithms the
+# insertion into a Frame (where the usual assignment happens) happens just
+# before writing.
+
+from Gaudi.Configuration import VERBOSE
+from Configurables import TestCollectionIDAssignment, ExampleFunctionalProducerMultiple
+
+from k4FWCore import ApplicationMgr
+from Configurables import EventDataSvc
+
+producer = ExampleFunctionalProducerMultiple(
+    "Producer", OutputCollectionParticles2=["CrazyNamesForCrazyHashes"], OutputLevel=VERBOSE
+)
+
+
+collid_checker = TestCollectionIDAssignment(
+    "CollectionIDChecker", InputCollection=["CrazyNamesForCrazyHashes"], OutputLevel=VERBOSE
+)
+
+ApplicationMgr(
+    TopAlg=[producer, collid_checker],
+    EvtSel="NONE",
+    EvtMax=1,
+    ExtSvc=[EventDataSvc()],
+)
