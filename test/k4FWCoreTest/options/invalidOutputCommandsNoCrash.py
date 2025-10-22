e377fea694f3069aced2cf8ad803b5eec912941a
Thomas Madlener <thomas.madlener@desy.de>
2025-10-22 19:01:44 +0200

    Include license notice

--- test/k4FWCoreTest/options/invalidOutputCommandsNoCrash.py
+++ test/k4FWCoreTest/options/invalidOutputCommandsNoCrash.py
@@ -16,6 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 from Gaudi.Configuration import INFO
 from k4FWCore import ApplicationMgr, IOSvc
