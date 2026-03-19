d2f84013e6f2bd82042cd45ca303dc225792e9da
scott-snyder <snyder@bnl.gov>
2026-03-19 10:18:05 +0000

    Adding a method to INoiseConstTool to return both RMS/offset. (#383)

--- k4Interface/include/k4Interface/INoiseConstTool.h
+++ k4Interface/include/k4Interface/INoiseConstTool.h
@@ -33,8 +33,23 @@ class INoiseConstTool : virtual public IAlgTool {
 public:
   DeclareInterfaceID(INoiseConstTool, 1, 0);
 
+  /** Expected noise per cell in terms of sigma of Gaussian distibution.
+   *   @param[in] aCellId of the cell of interest.
+   *   return double.
+   */
   virtual double getNoiseRMSPerCell(uint64_t aCellID) const = 0;
+
+  /** Expected noise per cell in terms of mean of distibution.
+   *   @param[in] aCellId of the cell of interest.
+   *   return double.
+   */
   virtual double getNoiseOffsetPerCell(uint64_t aCellID) const = 0;
+
+  /** Expected noise per cell.
+   *   @param[in] aCellId of the cell of interest.
+   *   return [rms, offset]
+   */
+  virtual std::pair<double, double> getNoisePerCell(uint64_t aCellID) const = 0;
 };
 
 #endif /* RECINTERFACE_INOISECONSTTOOL_H */
