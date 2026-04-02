9529ff40a7b80f0dfdf26a2b8c171391bce6fb5f
Thomas Madlener <thomas.madlener@desy.de>
2026-04-02 20:45:21 +0200

    Add utiltity functions for storing collection parameters

--- k4FWCore/include/k4FWCore/MetadataUtils.h
+++ k4FWCore/include/k4FWCore/MetadataUtils.h
@@ -105,6 +105,27 @@ getParameter(const std::string& name) {
   return metadataSvc->get<T>(name);
 }
 
+/// @brief Put a metadata parameter associated with a collection into the metadata
+///
+/// Internally builds the correct parameter name from the collection name and
+/// parameter name and then stores the value into the metadata frame via the
+/// MetadataSvc
+///
+/// @param collName The name of the collection to which the parameter should be
+///        associated
+/// @param paramName The name of the parameter
+/// @param value The value of the parameter
+/// @param comp The Gaudi component (algorithm, tool) that is saving the
+///             parameter, typically "this"
+/// @tparam T The type of the parameter value
+/// @tparam GaudiComp The type of the component. This will be deduced in
+///                   pretty much all of the use cases
+template <typename T, typename GaudiComp = Gaudi::Algorithm>
+void putCollectionParameter(const std::string& collName, const std::string& paramName, const T& value,
+                            const GaudiComp* comp) {
+  putParameter(podio::collMetadataParamName(collName, paramName), value, comp);
+}
+
 /// @brief Put the CellID encoding string for a collection into the metadata
 ///
 /// Internally builds the correct parameter name and then stores the encoding
@@ -137,6 +158,27 @@ void putCellIDEncoding(const std::string& collName, const std::string& encoding,
 template <typename GaudiComp = Gaudi::Algorithm>
 std::optional<std::string> getCellIDEncoding(const std::string& collName, const GaudiComp* comp) {
   return getParameter<std::string>(podio::collMetadataParamName(collName, edm4hep::labels::CellIDEncoding), comp);
+}
+
+/// @brief Get a metadata parameter associated with a collection from the metadata
+///
+/// Internally builds the correct parameter name from the collection name and
+/// parameter name and then retrieves the value from the metadata frame via the
+/// MetadataSvc
+///
+/// @param collName The name of the collection for which the parameter should be
+///                 retrieved
+/// @param paramName The name of the parameter
+/// @param comp The Gaudi component (algorithm, tool) that is retrieving the
+///             parameter, typically "this"
+/// @tparam T The type of the parameter value
+/// @tparam GaudiComp The type of the component. This will be deduced in
+///                   pretty much all of the use cases
+/// @return The parameter value if it has been found or std::nullopt if not
+template <typename T, typename GaudiComp = Gaudi::Algorithm>
+std::optional<T> getCollectionParameter(const std::string& collName, const std::string& paramName,
+                                        const GaudiComp* comp) {
+  return getParameter<T>(podio::collMetadataParamName(collName, paramName), comp);
 }
 
 } // namespace k4FWCore
