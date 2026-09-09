6d44fc12d0b6094f6280257f1edfd60104624446
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-09-09 08:35:32 +0200

    Complete functional test component refactor

--- test/k4FWCoreTest/options/readMarlinWrapperCollection.py
+++ test/k4FWCoreTest/options/readMarlinWrapperCollection.py
@@ -20,11 +20,11 @@
 # This is an example producing, transforming and consuming data in memory
 
 from Gaudi.Configuration import INFO
-from Configurables import ExampleFunctionalConsumer, ExampleFunctionalProducer
+from Configurables import ExampleFunctionalConsumer, ExampleFunctionalProducerWithMarlinWrapper
 from k4FWCore import ApplicationMgr
 from Configurables import EventDataSvc
 
-producer = ExampleFunctionalProducer("Producer")
+producer = ExampleFunctionalProducerWithMarlinWrapper("Producer")
 consumer = ExampleFunctionalConsumer(
     "Consumer",
     InputCollection="MCParticles",
