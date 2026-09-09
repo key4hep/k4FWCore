e16ffa7f905eab82571b53474aab41845a8cc791
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-09-09 08:51:01 +0200

    ci: fix pre-commit checks

--- test/k4FWCoreTest/options/TestAlgorithmWithTFile.py
+++ test/k4FWCoreTest/options/TestAlgorithmWithTFile.py
@@ -21,7 +21,6 @@ from Gaudi.Configuration import INFO
 from k4FWCore import ApplicationMgr, IOSvc
 from Configurables import k4FWCoreTest_AlgorithmWithTFile, EventDataSvc
 
-
 producer = k4FWCoreTest_AlgorithmWithTFile()
 
 iosvc = IOSvc()
